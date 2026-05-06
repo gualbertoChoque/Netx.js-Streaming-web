6a6940162012fc59b7eb8f0af1012f297c5ba502
Gualberto Choque <choque2015li@gmail.com>
2026-05-06 00:19:13 -0400

    Primer componente

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Button from "@/components/Button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button onclick={() => alert("Hola estudiantes")}>Hasme Clic</Button>
+    </div>
+  );
+}
---
+++ components/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+type ButtonProps = {
+  children: React.ReactNode;
+  onclick: () => void;
+};
+
+export default function Button({ children, onclick }: ButtonProps) {
+  return (
+    <button onClick={onclick} className="bg-blue-600 text-white px-4 py-2">
+      (children)
+    </button>
+  );
+}
